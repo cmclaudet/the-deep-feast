f86da87f9e5bc0d745d86690ca076bda7e7f5fe4
Catherine Claudet <cmclaudet@gmail.com>
2025-10-11 18:18:35 -0400

    Add some basic fish

---
+++ Components/ControlPanel.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class ControlPanel : ColorRect
+{
+	// add trigger feed fish button
+}
---
+++ Components/Fish.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class Fish : CharacterBody2D
+{
+	//floating around
+	//will eat food
+}
